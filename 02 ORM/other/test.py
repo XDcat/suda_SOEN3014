7cff77be4ad378900642b70f498b9e9925d489ef
XD <514269236@qq.com>
2019-10-09 15:47:12 +0800

    update ignore

--- 02 ORM/other/test.py
+++
@@ -1,5 +0,0 @@
-import re
-
-a = '9,《入理解计算机系统（原书第2版）》,9.7,1196,（美）Randal E.Bryant / David,机械工业出版社 / 2011-1-1 / 99.00元\n10,《入理解计算机系统（原书第2版）》,9.7,1196,（美）Randal E.Bryant / David,机械工业出版社 / 2011-1-1 / 99.00元\n'
-text = re.sub("%d,.*" % 10, "test", a)
-print(text)
